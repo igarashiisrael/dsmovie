404493d52a44a1d1a5446b64d659952fd1753350
Israel <igarashiisrael@icloud.com>
2022-05-15 19:01:33 -0300

    update color and link

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -12,7 +12,7 @@ function Navbar() {
           <a href="https://github.com/igarashiisrael" target="_blank" rel="noreferrer">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/igarashiisrael</p>
             </div>
           </a>
         </div>
